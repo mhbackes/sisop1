f1b1682ccb233bd6786046d8fe82f0008c008638
Luis Pedro Silvestrin <lpsilvestrin@inf.ufrgs.br>
2015-04-29 15:25:49 -0300

    merge

--- mthread/testes/mcreate.c
+++ mthread/testes/mcreate.c
@@ -8,13 +8,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "../include/mthread.h"
+#include "../include/mdata.h"
 
 void print_thread(){
 	printf("Olá, sou uma thread!");
 }
 
 int main() {
-	mcreate(0, &print_thread, NULL);
-
+	TCB_t* teste1 = thread_init(0, 1, 1, &print_thread, NULL);
+	TCB_t* teste2 = thread_init(2, 1, 1, &print_thread, NULL);
+	TCB_t* teste3 = thread_init(3, 1, 1, &print_thread, NULL);
+	TCB_t* teste4 = thread_init(4, 1, 1, &print_thread, NULL);
+	insert_blocked_thread(0, teste2);
+	insert_blocked_thread(3, teste4);
+	insert_blocked_thread(4, teste3);
+	insert_blocked_thread(2, teste1);
+	
 	return 0;
 }
