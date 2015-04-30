c62c599443a03c66a36c42b0304cb86a54954c41
Luis Pedro Silvestrin <lpsilvestrin@inf.ufrgs.br>
2015-04-30 15:57:39 -0300

    yield mudado

--- mthread/src/mthread.c
+++ mthread/src/mthread.c
@@ -12,18 +12,12 @@ int mcreate(int prio, void *(*start)(void*), void *arg) {
 }
 
 int myield() {
-	int yielded = 0;
-	int ret = getcontext(&(_run_head->context));
-
-	if (yielded == 0) {
-		yielded = 1;	// evita que a thread execute esse código quando voltar
-		int prio = _run_head->prio;
-		TCB_t *yielded_thread = dequeue(&_run_head, &_run_tail);
-		yielded_thread->state = READY;
-		enqueue(&_ready_head[prio], &_ready_tail[prio], yielded_thread);
-		setcontext(&_sched_context); // chaveia para o escalonador
-	}
-	return ret; // caso ocorra algum erro em getcontext, retorna -1, senão, retorna 0
+	int prio = _run_head->prio;
+	TCB_t *yielded = dequeue(&_run_head, &_run_tail);
+	yielded->state = READY;
+	enqueue(&_ready_head[prio], &_ready_tail[prio], yielded);
+	int ret = swapcontext(&(yielded->context), &_sched_context);
+	return ret;
 }
 
 int mmutex_init(mmutex_t *mtx) {
