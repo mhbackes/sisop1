85a254d8374351c6e4458be6db36ab2abe3e80bc
Luis Pedro Silvestrin <lpsilvestrin@inf.ufrgs.br>
2015-06-11 17:06:15 -0300

    reorganizando arquivos

--- t2fs/apidisk.h
+++
@@ -1,6 +0,0 @@
-
-#ifndef SECTOR_SIZE
-#define SECTOR_SIZE 256
-int read_sector (unsigned int sector, char *buffer);
-int write_sector (unsigned int sector, char *buffer);
-#endif
--- t2fs/t2fs.c
+++
@@ -1,8 +0,0 @@
-#include "t2fs.h"
-#include <string.h>
-
-int identify(char *name, int size) {
-	strncpy(name, "Luis Pedro Silvestrin xxxxxxxx Marcos Henrique Backes 228483", size);
-	name[size - 1] = '\0';
-	return 0;
-}
